72347ca0fd038809292c54ec21e19846177418b8
Ulrich <juyagu@gmail.com>
2020-06-10 20:25:57 -0300

    Cambios en proyecto

--- AbmAlumnosWeb/Abm.Master.designer.cs
+++ AbmAlumnosWeb/Abm.Master.designer.cs
@@ -51,6 +51,15 @@ namespace AbmAlumnosWeb
         protected global::System.Web.UI.WebControls.HyperLink HyperLinkListado;
 
         /// <summary>
+        /// Control HyperLinkCursos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkCursos;
+
+        /// <summary>
         /// Control ContentPlaceHolder1.
         /// </summary>
         /// <remarks>
---
+++ AbmAlumnosWeb/AdministracionCursos.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Abm.Master" AutoEventWireup="true" CodeBehind="AdministracionCursos.aspx.cs" Inherits="AbmAlumnosWeb.AdministracionCursos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Administración de cursos</h1>
+</asp:Content>
